dd8a08ead58f625e3a39a23af0b223a08ccc8615
usuallycwdillon <cwdillon@gmail.com>
2014-10-23 20:22:32 -0400

    Now with results, a figure and a table

--- Garbage Can Model/analysis/gcc-analysis.R
+++ Garbage Can Model/analysis/gcc-analysis.R
@@ -18,33 +18,63 @@ library(lattice)
 library(reshape2)
 library(ggplot2)
 library(plyr)
+library(lattice)
 
 ## Data
 #  Establish the data directory relative to the working directory, then id
 #  some column names 
 dataDir <- "Garbage Can Model/output/" 
 
-# Main Experiment => me
-me <- read.csv(paste(dataDir,
-                       "GCC Main Experiment - National Priorities - 2014-05-04 154654_mod.csv",
-                          sep=""),
-                    strip.white = TRUE,
-                    header=TRUE,
-                    sep=",",
-               colClasses=c(job.ID="factor",
+# Main Experiment With Parties => me.party
+me.party <- read.csv("Garbage Can Model/output/dated/GCC Main Experiment - National Priorities - 2014-05-04 154654_mod.csv",
+               strip.white = TRUE,
+               header=TRUE,
+               sep=",",
+               colClasses=c(timestamp="character",
+                            job.ID="factor",
                             setUnaffiliatedFraction="factor",
                             setGreenFraction="factor",
                             setStatePriorities="factor",
-                            setIdeologyIssues="factor") 
+                            setIdeologyIssues="factor"
+                            )
                )
 # Main Experiment, No Parties => me.np
-me.np <- read.csv(paste(dataDir,
-                     "GCC Main Experiment - National Priorities - No Parties 2014-05-03 175733_mod.csv",
-                     sep=""),
-               strip.white = TRUE,
-               header=TRUE,
-               sep=",")
+me.np <- read.csv("Garbage Can Model/output/dated/GCC Main Experiment - National Priorities - No Parties 2014-05-03 175733_mod.csv",
+                 strip.white = TRUE,
+                 header=TRUE,
+                 sep=",",
+                 colClasses=c(timestamp="character",
+                              setStatePriorities="factor",
+                              setIdeologyIssues="factor"
+                              )
+                 )
 
+me.np$job.ID <- me.np$job.ID + 24
+me.np$job.ID <- as.factor(me.np$job.ID)
+me.np$setGreenFraction <- "0"
+me.np$setUnaffiliatedFraction <- "1"
+
+me <- rbind(me.party, me.np)
+
+summary(me)
+
+bwplot(me$laws.count ~ me$provisions | me$setUnaffiliatedFraction + me$setGreenFraction + me$setStatePriorities + me$setIdeologyIssues)
+
+bwplot(laws.count ~ provisions | setUnaffiliatedFraction + setStatePriorities + setIdeologyIssues, me.np)
+
+qqmath(me$laws.count ~ me$provisions | me$setUnaffiliatedFraction + me$setGreenFraction + me$setStatePriorities + me$setIdeologyIssues)
+
+densityplot(me$laws.count ~ me$provisions | me$setUnaffiliatedFraction + me$setGreenFraction + me$setStatePriorities + me$setIdeologyIssues)
+
+histogram(me$laws.count ~ me$provisions | me$setUnaffiliatedFraction + me$setGreenFraction + me$setStatePriorities + me$setIdeologyIssues)
+
+barchart(me$laws.count ~ me$provisions | me$setUnaffiliatedFraction + me$setGreenFraction + me$setStatePriorities + me$setIdeologyIssues)
+
+dotplot(me$laws.count ~ me$provisions | me$setUnaffiliatedFraction + me$setGreenFraction + me$setStatePriorities + me$setIdeologyIssues)
+
+stripplot(me$laws.count ~ me$provisions | me$setUnaffiliatedFraction + me$setGreenFraction + me$setStatePriorities + me$setIdeologyIssues)
+
+tmd(me$laws.count ~ me$provisions | me$setUnaffiliatedFraction + me$setGreenFraction + me$setStatePriorities + me$setIdeologyIssues)
 
 tv <- ggplot(me, aes(reorder(job.ID, total.votes, FUN=median), total.votes)) +  
   geom_boxplot(aes(reorder(job.ID, total.votes, FUN=median), total.votes, alpha=0)) + 
@@ -63,11 +93,12 @@ laws <- ggplot(me, aes(reorder(job.ID, laws.count, FUN=median), laws.count)) +
   geom_jitter (alpha=I(1/5)) + 
   stat_summary(fun.data = "mean_se", colour = "red") + 
   theme(axis.title.y=element_text(angle=0)) +
-  labs(title="Quantiles with Jittered Total Laws Passed by Job, Sorted by Median Values ") +
-  xlab("Jobs, by ID") + 
+  labs(title="Total Laws Passed by Case: \nJitters showing quintiles & sorted by median values (red)") +
+  theme(legend.position = "none") +
+  xlab("Cases by ID") + 
   ylab("New \nLaws")
 laws
-ggsave(laws, file="laws_byJob_jitterQuants.png")
+ggsave(laws, file="Garbage Can Model/analysis/plots/laws_byJob_jitterQuints.png")
 
 
 lawsf1 <- ggplot(me, 
@@ -99,7 +130,7 @@ ggsave(lawsf2, file="laws_byJob_byUnaffiliated_byGreen.png")
 
 # A smaller verion of the main experiment dataset; 
 # jobs 1, 3, 5, 9, 13, 14, 15, 16, 17, 21 have been removed.
-nonzero.jobs <- c(2, 4, 6, 7, 8, 10, 12, 18, 19, 20, 22, 23, 24)
+nonzero.jobs <- as.factor(c(2, 4, 6, 7, 8, 10, 11, 12, 18, 19, 20, 22, 23, 24))
 mes <- subset(me, 
               select=c(job.ID, 
                      provisions, 
@@ -113,8 +144,9 @@ mes <- subset(me,
                      setIdeologyIssues),
               subset=(job.ID %in% nonzero.jobs))
 
-mes$job.f <- factor(mes$job.ID, levels=c("2", "4", "6", "7", "8", "10", "12",
-                                           "18", "19", "20", "22", "23", "24"))
+mes$job.f <- factor(mes$job.ID, levels=c("2", "4", "6", "7", "8", "10", "11",
+                                         "12", "18", "19", "20", "22", "23",
+                                         "24"))
 
 prov.sat <- qplot(provisions, satisfaction, data=mes,
              geom="point", alpha = I(1/5), 
@@ -139,7 +171,7 @@ prov.votes <- qplot(provisions, total.votes, data=mes,
                   geom="point", alpha = I(1/5), 
                   main="Total Votes by Number of Additional Provisions \nfor Select Jobs",
                   xlab = "Number of Additional Provisions by (top-to-bottom): \nJob ID, # State Priorities, #Ideology Issues, % Unaffiliated, %Green Party", 
-                  ylab = "Total \nVotes") +
+                  ylab = "Total \nVotes", scales = "free", space = "free") +
   geom_smooth(method = "lm", se = T) +
   theme(axis.title.y=element_text(angle=0), 
         axis.text.y=element_text(size=5.5)) +
@@ -189,10 +221,28 @@ red.mrme <- step(mrme, direction="backward")
 # based on the stepwise comparisson, I'm backing out the total.votes (probably
 # autocorrelated with laws.count, satisfaction and setIdeologyIssues)
 summary(red.mrme)
-
 summary(mrme)
 
 confint(mrme, level=0.99)
+
+###############################################################################
+# 
+# We now have a theory that the total votes and laws passed depend on whether
+# there are state priorities, ideology issues, the unaffiliated fraction and 
+# the Green fraction. Let's try a regression model on those...
+#
+###############################################################################
+lm4 <- lm(me$total.votes ~ me$setUnaffiliatedFraction * 
+            me$setGreenFraction * me$setStatePriorities * me$setIdeologyIssues)
+summary(lm4)
+
+require(dplyr)
+law.sum <- group_by(me, job.ID, setUnaffiliatedFraction, setGreenFraction,
+                    setStatePriorities, setIdeologyIssues)
+law.des <- summarise(law.sum)
+law.des
+
+
 
 
 ## Citations for R and R packages used here
